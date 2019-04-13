6cd8f2da4f1653156c3641d134a58caaf3c01363
Eric Anderson <eanders@microsoft.com>
2019-04-12 19:32:53 -0700

    add .env file support (for running on mre vm)

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,16 @@
  * Licensed under the MIT License.
  */
 
-import { log, WebHost } from '@microsoft/mixed-reality-extension-sdk';
+import { WebHost } from '@microsoft/mixed-reality-extension-sdk';
+import dotenv from 'dotenv';
 import { resolve as resolvePath } from 'path';
 import WearAHat from './app';
 
-log.enable('app');
-
 process.on('uncaughtException', err => console.log('uncaughtException', err));
 process.on('unhandledRejection', reason => console.log('unhandledRejection', reason));
+
+// Read .env if file exists
+dotenv.config();
 
 // Start listening for connections, and serve static files
 const server = new WebHost({
